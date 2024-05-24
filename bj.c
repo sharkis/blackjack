e3eccb69d48e66956e8815de328478853bc870be
Albert Sharkis <sharkis@sharkis.org>
2024-05-24 16:49:19 -0600

    license

--- bj.c
+++ bj.c
@@ -2,16 +2,20 @@
 #include "utf8_encode.h"
 
 int main() {
-  int start = 0x1f0a1;
-  char out[5];
-  for(int k=0; k<4;k++){
-    for (int i = 0; i < 14; i++) {
-      if(i==11) continue;
-      utf8_encode(out, start + i);
-      printf("%s ", out);
-    }
-    printf("\n");
-    start = start + 0x10;
-  }
-  return 0;
+	printf("blackjack Copyright (C) 2024  Albert Sharkis\n\
+This program comes with ABSOLUTELY NO WARRANTY\n\
+This is free software, and you are welcome to redistribute it\n\
+under certain conditions.\n\n");
+	int start = 0x1f0a1;
+	char out[5];
+	for(int k=0; k<4;k++){
+		for (int i = 0; i < 14; i++) {
+			if(i==11) continue;
+			utf8_encode(out, start + i);
+			printf("%s ", out);
+		}
+		printf("\n");
+		start = start + 0x10;
+	}
+	return 0;
 }
