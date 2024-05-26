9262c37412f36589acc592e2df286128b99200f2
Albert Sharkis <sharkis@sharkis.org>
2024-05-26 14:22:33 -0600

    generate dealer deck of 312 cards, still need to shuffle

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all : utf8_encode.o
-	gcc -o bj bj.c ./utf8_encode.o
+	gcc bj.c ./utf8_encode.o
 utf8encode.o : 
 	gcc -c utf8_encode.c
 clean :
-	rm ./bj ./*.o
+	rm ./a.out ./*.o
--- bj.c
+++ bj.c
@@ -1,37 +1,52 @@
-#include <stdio.h>
 #include "utf8_encode.h"
+#include <stdio.h>
 
 /*TODO
  * finish game set-up
  * betting
- * insurance 
+ * insurance
  */
 
-int cardvalue(int card){
+int cardvalue(int card) {
   int tvalue = card & 0xf;
   return tvalue > 10 ? 10 : tvalue;
 }
 
+void gendeck(int *deck) {
+  int start = 0x1F0A1; // ace o' spades
+  char out[5];         // four bytes for unicode output + null byte
+  for (int i = 0; i < 6; i++) {
+    for (int k = 0; k < 4; k++) {
+      for (int j = 0; j < 14; j++) {
+        int cardno = start + (k * 16) + j;
+        if (j >= 11)
+          cardno++;
+        deck[(i * 52) + (k * 13) + j] = cardno;
+      }
+    }
+  }
+}
+
 int main() {
   printf("blackjack Copyright (C) 2024  Albert Sharkis\n"
-      "This program comes with ABSOLUTELY NO WARRANTY\n"
-      "This is free software, and you are welcome to redistribute it\n"
-      "under certain conditions.\n\n");
-  int hand[21] = { 0 }; // biggest possible hand, 21 straight aces
-  int start = 0x1f0a1; // ace o' spades
-  char out[5]; // four bytes for unicode output
-  for(int k=0; k<4;k++){
-    for (int i = 0; i < 14; i++) {
-      if(i==11) continue;
-      utf8_encode(out, start + i);
-      printf("%s %d\t", out, cardvalue(start + i));
-    }
-    printf("\n");
-    start = start + 0x10;
-  }
+         "This program comes with ABSOLUTELY NO WARRANTY\n"
+         "This is free software, and you are welcome to redistribute it\n"
+         "under certain conditions.\n\n");
+  int hand[21] = {0}; // biggest possible hand, 21 straight aces
+  int deck[312] = {0};
+  // create deck 6 decks of 52 cards
+  gendeck(deck);
+  // TODO shuffle deck
 
-// create deck (6-8) decks of 52 cards
-// deal cards
-// prompt user
+  // TODO deal cards
+
+  char out[5]; // four bytes for unicode output + null byte
+  for (int i = 0; i < 312; i++) {
+    utf8_encode(out, deck[i]);
+    if (i % 52 == 0)
+      printf("\n");
+    printf("%s  ", out);
+  }
+  // TODO prompt user
   return 0;
 }
